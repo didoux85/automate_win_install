bd2e85173760ceeb0f4f5cffd3740af87016d6b7
didoux85 <74797632+didoux85@users.noreply.github.com>
2020-11-20 23:21:54 +0100

    Add files via upload

---
+++ Automation_BootInstall_AM.ps1
@@ -0,0 +1,11 @@
+﻿Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+reg add HKLM\SOFTWARE\Microsoft\.NETFramework\v4.0.30319 /v SchUseStrongCrypto /t REG_DWORD /d 1 /reg:64
+reg add HKLM\SOFTWARE\Microsoft\.NETFramework\v4.0.30319 /v SchUseStrongCrypto /t REG_DWORD /d 1 /reg:32
+Install-PackageProvider chocolatey
+Set-PackageSource -name chocolatey -trusted
+set string $message
+$name = Read-host -Prompt 'Nom du Logiciel'
+$message = Find-Package -Name *$name* -Source chocolatey
+Write-Host $message.name
+#Install-Package -Name adobereader,GoogleChrome,Firefox,office2019proplus, -Source chocolatey
+
